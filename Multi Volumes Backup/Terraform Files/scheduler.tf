198028574fffc2b71d3c5bddcdbae33237dc2499
oreade97 <143465426+oreade97@users.noreply.github.com>
2025-07-17 12:47:32 -0400

    Update scheduler.tf

--- Multi Volumes Backup/Terraform Files/scheduler.tf
+++ Multi Volumes Backup/Terraform Files/scheduler.tf
@@ -1,15 +1,3 @@
-/*resource "google_cloud_scheduler_job" "netapp_backup_job" {
-  name             = "netapp-backup-job"
-  description      = "Scheduled job to trigger NetApp volume backup"
-  schedule         = var.backup_schedule
-  time_zone        = "UTC"
-  pubsub_target {
-    topic_name = google_pubsub_topic.netapp_backup_topic.id
-    data       = base64encode("{\"message\": \"Trigger NetApp Volume Backup\"}")
-  }
-}
-*/
-
 resource "google_cloud_scheduler_job" "backup_scheduler" {
   name        = "netapp-backup-scheduler"
   schedule    = "0 * * * *"  # Every hour
