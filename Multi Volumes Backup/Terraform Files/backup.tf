cf26c54a4a3209990eb9c428a8466478a7394e14
oreade97 <143465426+oreade97@users.noreply.github.com>
2025-07-17 12:47:06 -0400

    Update backup.tf

--- Multi Volumes Backup/Terraform Files/backup.tf
+++ Multi Volumes Backup/Terraform Files/backup.tf
@@ -6,23 +6,3 @@ resource "google_netapp_backup_vault" "test_backup_vault" {
     "creator": "testuser"
   }
 }
-/*
-resource "google_netapp_backup_vault" "test_backup_vault1" {
-  name = "netapp-backup-vaulttwo"
-  location = "us-east4"
-  description = "Terraform created vault"
-  labels = { 
-    "creator": "testuser"
-  }
-}
-
-
-resource "google_netapp_backup_vault" "test_backup_vault2" {
-  name = "netapp-backup-vaultone"
-  location = "us-east4"
-  description = "Terraform created vault"
-  labels = { 
-    "creator": "testuser"
-  }
-}
-*/
